b31ed9d5d2b83194056f3280d265e107d7587d2d
thugiang <xanhtham.cuc@gmail.com>
2022-07-21 23:51:50 +1200

    set up next js and sanity

---
+++ components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Head from "next/head"; //like html meta in >head>. give someinfo about website
+
+const Layout = () => {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
